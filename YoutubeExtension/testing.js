226ff9baf13ae9ec45fbcd8d32c5a540a92e0371
Ryan Knauer <wannasplitapineapple@gmail.com>
2019-05-27 21:19:52 -0700

    Update testing.js

--- YoutubeExtension/testing.js
+++ YoutubeExtension/testing.js
@@ -21,17 +21,6 @@ var checkControls = function(){
         $('.ytp-right-controls').prepend(el)
     }
 }
-// (function() {
-//     // Load the script
-//     var script = document.createElement("SCRIPT");
-//     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-//     script.type = 'text/javascript';
-//     script.onload = function() {
-//         var $ = window.jQuery;
-//         checkControls()
-//     };
-//     document.getElementsByTagName("head")[0].appendChild(script);
-// })();
 
 
 jQuery(document).ready(function($){
